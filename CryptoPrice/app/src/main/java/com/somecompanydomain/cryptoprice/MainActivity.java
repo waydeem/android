0f3317461f5bc6d57622627cdfda69f77c06e69f
jippanippa <jippanippa@yandex.ru>
2017-09-19 16:50:04 +0300

    Add valuable changes

--- CryptoPrice/app/src/main/java/com/somecompanydomain/cryptoprice/MainActivity.java
+++ CryptoPrice/app/src/main/java/com/somecompanydomain/cryptoprice/MainActivity.java
@@ -10,10 +10,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,9 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
     private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
 
-    TextView mBtcPriceTextView;
-    TextView mEthPriceTextView;
-    TextView mLtcPriceTextView;
+    TextView mBtcAskPriceTextView;
+    TextView mEthAskPriceTextView;
+    TextView mLtcAskPriceTextView;
+    TextView mBtcBidPriceTextView;
+    TextView mEthBidPriceTextView;
+    TextView mLtcBidPriceTextView;
 
 
     @Override
@@ -34,13 +35,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBtcPriceTextView = (TextView) findViewById(R.id.btcPrice);
-        mEthPriceTextView = (TextView) findViewById(R.id.ethPrice);
-        mLtcPriceTextView = (TextView) findViewById(R.id.ltcPrice);
+        mBtcAskPriceTextView = (TextView) findViewById(R.id.btcPriceAsk);
+        mEthAskPriceTextView = (TextView) findViewById(R.id.ethPriceAsk);
+        mLtcAskPriceTextView = (TextView) findViewById(R.id.ltcPriceAsk);
+        mBtcBidPriceTextView = (TextView) findViewById(R.id.btcPriceBid);
+        mEthBidPriceTextView = (TextView) findViewById(R.id.ethPriceBid);
+        mLtcBidPriceTextView = (TextView) findViewById(R.id.ltcPriceBid);
         final Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+        setAdapterAndDataForSpinner(spinner);
 
-        //TODO: вывести в отдельную функцию всё нижестоящее безобразие
+    }
 
+    private void setAdapterAndDataForSpinner(final Spinner spinner) {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.currency_array, R.layout.spinner_item);
 
@@ -52,23 +58,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 spinner.setSelection(position);
-                getDataFromTheInternets(BASE_URL + "BTC" + spinner.getSelectedItem().toString());
-                getDataFromTheInternets(BASE_URL + "ETH" + spinner.getSelectedItem().toString());
-                getDataFromTheInternets(BASE_URL + "LTC" + spinner.getSelectedItem().toString());
+                getDataFromTheInternetsAndSetPrices(BASE_URL + "BTC" + spinner.getSelectedItem().toString());
+                getDataFromTheInternetsAndSetPrices(BASE_URL + "ETH" + spinner.getSelectedItem().toString());
+                getDataFromTheInternetsAndSetPrices(BASE_URL + "LTC" + spinner.getSelectedItem().toString());
                 Log.d("CRYPTO PRICE", spinner.getSelectedItem().toString());
 
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                Log.d("CRYPTO PRICE", "Nuthin' selected");
+                Log.d("CRYPTO PRICE", "Nuthin' was selected");
             }
         });
-
     }
 
-
-    private void getDataFromTheInternets(final String url){
+    private void getDataFromTheInternetsAndSetPrices(final String url){
 
         final AsyncHttpClient client = new AsyncHttpClient();
 
@@ -78,11 +82,14 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject jsonObjResponse) {
                 try {
                     if(url.contains("BTC")) {
-                        mBtcPriceTextView.setText(jsonObjResponse.getString("ask"));
+                        mBtcAskPriceTextView.setText(jsonObjResponse.getString("ask"));
+                        mBtcBidPriceTextView.setText(jsonObjResponse.getString("bid"));
                     } else if(url.contains("ETH")) {
-                        mEthPriceTextView.setText(jsonObjResponse.getString("ask"));
+                        mEthAskPriceTextView.setText(jsonObjResponse.getString("ask"));
+                        mEthBidPriceTextView.setText(jsonObjResponse.getString("bid"));
                     } else {
-                        mLtcPriceTextView.setText(jsonObjResponse.getString("ask"));
+                        mLtcAskPriceTextView.setText(jsonObjResponse.getString("ask"));
+                        mLtcBidPriceTextView.setText(jsonObjResponse.getString("bid"));
                     }
                 } catch (JSONException joE) {
                     joE.printStackTrace();
@@ -91,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String response, Throwable throwable) {
-                    Log.d("CRYPTO PRICE", "BAD RESPONCE " + url);
+                    Log.d("CRYPTO PRICE", "BAD RESPONCE " + url + " " + response);
                 }
         });
     }
